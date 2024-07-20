70ab90f3a677e36e3063a47c613d28b1e787404b
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-20 16:34:36 +0530

    added

---
+++ file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FILE=/etc/passwd
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+if [! -f $FILE] #! denotes oppsite
+then
+echo -e "$R source directory:$FILE does not exist $N"
+fi
+while IFS =":" read -r username passwd user_id group_id user_fullname home_dir shell_path
+do
+echo "username:$username"
+echo "user Id:$user_id"
+echo "user fullname:"

e5e885ebf21f72bdf8460b119c626ac573e9b7c0
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-23 11:53:03 +0530

    added

--- greeting.sh
+++ greeting.sh
@@ -4,7 +4,7 @@ echo "heloo $PERSON:good morning. we am learning shell script"
 NAME=""
 WISHES=""
 USAGE(){
-    echo "USAGE::$(basename $0)" -n <name> -w <wishes>
+    echo "USAGE::$(basename $0) -n <name> -w <wishes>"
     echo "options::"
     echo "-n, specify the name(mandatory)"
     echo "-w, specify the wishes,ex,good morning"

52a1efe0aee2484ae00a2bdde76a27030c81c8e6
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-23 12:03:51 +0530

    added

--- greeting.sh
+++ greeting.sh
@@ -10,3 +10,13 @@ USAGE(){
     echo "-w, specify the wishes,ex,good morning"
     echo "-h, display help and exit"
 }
+while getopts "n:w:h" opt;
+do
+    case $opt in
+        n)NAME="$OPTARGS";;
+        \?)echo "invalid options:"$OPTARGS"" >&2 USAGE exit
+         w)WISHES="$OPTARGS";;
+        h)USAGE; exit;;
+        :)USAGE; exit;;
+    esac    
+done
